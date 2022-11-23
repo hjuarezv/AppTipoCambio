abbb7cbc9b66f47ca811ebb1ac897d11b5e75b14
hjuarezv <hjuarezx@gmail.com>
2022-11-22 23:45:42 -0500

    App que utiliza REST-API para obtener el tipo de cambio

---
+++ AppTipoCambio/AppTipoCambio/Model/TipoCambio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppTipoCambio.Model
+{
+    public class TipoCambio
+    {
+        public double compra { get; set; }
+        public double venta { get; set; }
+        public string origen { get; set; }
+        public string moneda { get; set; }
+        public DateTime fecha { get; set; }
+    }
+}
---
+++ AppTipoCambio/AppTipoCambio/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,51 @@
+﻿using AppTipoCambio.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AppTipoCambio.ViewModel
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        private TipoCambio dolar1;
+
+        public TipoCambio dolar
+        {
+            get => dolar1; 
+            set
+            {
+                dolar1 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainPageViewModel()
+        {
+            GetTipoCambio();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private async void GetTipoCambio()
+        {
+            string url = "https://api.apis.net.pe/v1/tipo-cambio-sunat";
+            HttpClient cliente = new HttpClient();
+            HttpResponseMessage respuesta = await cliente.GetAsync(url);
+            var json = await respuesta.Content.ReadAsStringAsync();
+            //Debug.WriteLine(json);
+            dolar = JsonConvert.DeserializeObject<TipoCambio>(json);
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
